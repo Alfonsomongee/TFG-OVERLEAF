833b4942588132c2a2abf573643ea4762f0659e8
Alfonso Monge Diaz-Angel <tu.email@us.es>
2026-06-02 20:50:35 +0200

    fix: revertir workboxOptions incompatible con Docusaurus 2.4.3

--- tfg-antigravity-docs/docusaurus.config.js
+++ tfg-antigravity-docs/docusaurus.config.js
@@ -90,19 +90,6 @@ const config = {
         'standalone',
         'queryString',
       ],
-      workboxOptions: {
-        navigateFallback: null,
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/tfg-overleaf\.vercel\.app\/(en|de)\//,
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'i18n-pages',
-              networkTimeoutSeconds: 3,
-            },
-          },
-        ],
-      },
       pwaHead: [
         {
           tagName: 'link',
