52f884531992f3c2e2faa6039f497078a27cd7a1
Alfonso Monge Diaz-Angel <tu.email@us.es>
2026-06-13 20:08:30 +0200

    fix: import useState in CecoelSimulator.jsx to prevent crash

--- tfg-antigravity-docs/src/components/CecoelSimulator/CecoelSimulator.jsx
+++ tfg-antigravity-docs/src/components/CecoelSimulator/CecoelSimulator.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer, useEffect, useRef } from 'react';
+import React, { useReducer, useEffect, useRef, useState } from 'react';
 import Translate, { translate } from '@docusaurus/Translate';
 import styles from './CecoelSimulator.module.css';
 
